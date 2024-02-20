f9639534b2e1ca03724da5800ec9e04373455ac6
Ngansan <21521174@gm.uit.edu.vn>
2024-02-21 00:11:17 +0700

    done interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
 import './App.css';
 import chatBotLogo from './assets/chatbotLogo.png';
 import './style/sidebar.scss';
+import './style/chat.scss';
 import { IoAddCircleOutline } from "react-icons/io5";
 import { IoHomeOutline } from "react-icons/io5";
 import { IoSaveOutline } from "react-icons/io5";
 import { GrUpgrade } from "react-icons/gr";
 import { FaRegMessage } from "react-icons/fa6";
+// import { Link } from 'react-router-dom';
+import { IoIosSend } from "react-icons/io";
+// import { TbMessageChatbot } from "react-icons/tb";
+// import { RxAvatar } from "react-icons/rx";
+import { sendMsgToOpenAI } from './openAI';
+import { useState } from 'react';
+import user from './assets/user.png';
+
 
 
 function App() {
+  const [input, setInput] = useState("");
+
+  
+  const [messages, setMessages] = useState([{
+    text: "Hi , I am ChatGPT, a state-of-",
+    isBot: true,
+  }]);
+
+
+  const handleSend = async () => {
+    const text = input;
+    setInput('');
+    setMessages([
+      ...messages,
+      {text, isBot: false}
+    ]);
+
+    const res = await sendMsgToOpenAI(input);
+    setMessages([
+      ...messages,
+      {
+        text, isBot: false
+      },
+      {text: res, isBot: true}
+    ]);
+  }; 
   return (
     <div className="App">
       <div className="sideBar">
@@ -32,7 +67,23 @@ function App() {
         </div>
       </div>
 
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          {messages.map((message, i) =>
+              <div key={i} className={message.isBot ? "chat second_chat" : "chat"}>
+                  <img src={message.isBot ? chatBotLogo : user} className='chatIcon'/>
+                  <p className="txt">{message.text}</p>
+              </div>
+          )}
+        </div>
+
+        <div className="chatFooter">
+            <div className="inp">
+              <input type="text" name="" value={input} onChange={(e) => {setInput(e.target.value)}} placeholder='Send a message' className='inp_input' onClick={handleSend}/>
+              <button className='send'><IoIosSend className='sendIcon'/></button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
